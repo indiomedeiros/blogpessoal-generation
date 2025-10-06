08a0d14539dac0e8887d2acfc2af27b248e9e27c
Índio Medeiros <71137294+indiomedeiros@users.noreply.github.com>
2025-10-06 16:26:00 -0300

    fixa erro de typeROM para deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,13 @@ import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
-import { DevService } from './data/services/dev.service';
+import { ProdService } from './data/services/prod.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: DevService,
+      useClass: ProdService,
       imports: [ConfigModule],
     }),
     PostagemModule,
